f8d1852a5cb9ff0bbf7fc967adb84df3465aafe6
JealynWu <1004177537@qq.com>
2022-11-24 15:39:47 +0800

    1，优化Json数据分享，导入。怎加相关接口，代码示例，注释说明。

--- app/src/main/java/com/linkiing/fdsmeshlibdemo/ui/MainActivity.kt
+++ app/src/main/java/com/linkiing/fdsmeshlibdemo/ui/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.godox.sdk.api.FDSMeshApi
 import com.godox.sdk.tool.HttpUtils
 import com.google.gson.Gson
-import com.google.gson.JsonSyntaxException
 import com.hjq.permissions.OnPermissionCallback
 import com.linkiing.fdsmeshlibdemo.R
 import com.linkiing.fdsmeshlibdemo.adapter.StudioAdapter
@@ -19,12 +18,14 @@ import com.linkiing.fdsmeshlibdemo.utils.FileSelectorUtils
 import com.linkiing.fdsmeshlibdemo.utils.FileUtils
 import com.linkiing.fdsmeshlibdemo.utils.PermissionsUtils
 import com.linkiing.fdsmeshlibdemo.view.dialog.InputTextDialog
+import com.linkiing.fdsmeshlibdemo.view.dialog.LoadingDialog
 import com.telink.ble.mesh.util.LOGUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     private lateinit var studioAdapter: StudioAdapter
     private lateinit var inputTextDialog: InputTextDialog
+    private lateinit var loadingDialog: LoadingDialog
     private var jsonStr = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,18 +49,33 @@ class MainActivity : BaseActivity() {
             goActivity(SettingActivity::class.java,false)
         }
 
+        loadingDialog = LoadingDialog(this)
+
         inputTextDialog = InputTextDialog(this)
         inputTextDialog.setTitleText("新增Studio")
         inputTextDialog.setOnDialogListener {
-            //新增Studio
+
             val studioListBean = StudioListBean(studioAdapter.getStudioNextIndex())
             studioListBean.name = it
             if (TextUtils.isEmpty(jsonStr)) {
+                //新增Studio
                 studioListBean.meshJsonStr = FDSMeshApi.instance.getInitMeshJson()
+                studioAdapter.addStudio(studioListBean)
             } else {
-                studioListBean.meshJsonStr = jsonStr
+                //导入json数据
+                loadingDialog.showDialog()
+                updateProvisionerAddress(jsonStr) { newJson ->
+                    runOnUiThread{
+                        loadingDialog.dismissDialog()
+                        if (!TextUtils.isEmpty(newJson)) {
+                            studioListBean.meshJsonStr = newJson
+                            studioAdapter.addStudio(studioListBean)
+                        } else {
+                            ConstantUtils.toast(this,"导入失败！")
+                        }
+                    }
+                }
             }
-            studioAdapter.addStudio(studioListBean)
         }
     }
 
@@ -78,11 +94,10 @@ class MainActivity : BaseActivity() {
                     if (!it.choose && !TextUtils.isEmpty(it.meshJsonStr)) {
                         //需要切换Mesh网络
                         LOGUtils.e("切换网络 =============> ${it.name}")
-                        importMeshJson(it)
-                    } else {
-                        goActivity(StudioActivity::class.java, "index",it.index,false)
+                        FDSMeshApi.instance.importMeshJson(it.meshJsonStr)
                     }
                     studioAdapter.changeChoose(it)
+                    goActivity(StudioActivity::class.java, "index",it.index,false)
                 }
 
                 override fun onDenied(permissions: MutableList<String>?, never: Boolean) {
@@ -117,15 +132,28 @@ class MainActivity : BaseActivity() {
         }
     }
 
-    private fun importMeshJson(studioListBean: StudioListBean) {
-
+    /**
+     * 更新json的ProvisionAddress
+     * 步骤：1，通过后台#queryAndSync#接口同步获取到唯一的《provisionerAddress》
+     *      2，通过 FDSMeshApi 的 #updateMeshJsonProvisionerAddress()# 方法修改json信息，并保存数据，下次中可直接使用。
+     *      3，通过 的 #importMeshJson()#方法导入并应用mesh网络
+     *
+     *     （2，3可使用 #updateAndImportMeshJson()# 方法合并为一步执行，导入成功后可通过 #getCurrentMeshJson()# 方法获取json数据保存）
+     *
+     * 注：只需要在第一次导入其他app分享的json数据时，才同需要同步服务端的provisionerAddress修改到json中。
+     *    之后保存修改后的json可直接使用。
+     *    同一台手机相同的json信息，每次从后台请求的provisionerAddress相同，避免多次调用给后台制造压力。
+     */
+    private fun updateProvisionerAddress(jsonStr: String, updateProvisionerAddressListener: (String) -> Unit) {
         //从json中获取信息
-        val meshJsonInfo = FDSMeshApi.instance.getMeshJsonInfoByJson(studioListBean.meshJsonStr)
-        LOGUtils.i("HTTP importMeshJson ==> netWorkKey:${meshJsonInfo.netWorkKey}  provisionAddress:${meshJsonInfo.provisionAddress}")
+        val meshJsonInfo = FDSMeshApi.instance.getMeshInfoByJson(jsonStr)
+        LOGUtils.i("HTTP importMeshJson ==> netWorkKey:${meshJsonInfo.netWorkKey}  provisionAddress:${meshJsonInfo.provisionerAddress}")
 
-        //获取服务器上的provisionAddress
-        val getProvisionAddressUrlStr = "http://godox.light.belvie-iot.com/api/provision/getByUuidAndSync?" +
-                "mesh_uuid=${meshJsonInfo.netWorkKey}&address=${meshJsonInfo.provisionAddress}"
+        //从服务器获取分配的provisionerAddress
+        val getProvisionAddressUrlStr = "http://192.168.2.102:8118/api/provision/queryAndSync?" +
+                "app_uuid=${FDSMeshApi.instance.getAppLocalUUID()}" +
+                "&network_key=${meshJsonInfo.netWorkKey}" +
+                "&address=${meshJsonInfo.provisionerAddress}"
         HttpUtils().httpRequest(this,HttpUtils.GET,getProvisionAddressUrlStr){
             LOGUtils.i("HTTP getProvisionAddressUrlStr ==> it:$it")
 
@@ -134,22 +162,21 @@ class MainActivity : BaseActivity() {
                     //请求到服务器的provisionAddress，根据provisionAddress导入json
                     val httpProvisionBean = Gson().fromJson(it,HttpProvisionBean::class.java)
                     LOGUtils.i("HTTP ==> address:${httpProvisionBean.data.address}")
-                    var serverProvisionAddress = 0
-                    if (httpProvisionBean.code == 0) {
-                        //服务器有当前mesh的地址分配记录
-                        serverProvisionAddress = httpProvisionBean.data.address
-                    }
 
-                    if (FDSMeshApi.instance.importMeshJson(studioListBean.meshJsonStr,serverProvisionAddress)) {
-                        goActivity(StudioActivity::class.java, "index",studioListBean.index,false)
-                    } else {
-                        LOGUtils.e("Error! 导入json失败！serverProvisionAddress:$serverProvisionAddress")
+                    var newMeshJson = jsonStr
+                    if (httpProvisionBean.code == 0) {
+                        //获取到ProvisionerAddress，更新到要导入的JSON文件中
+                        val serverProvisionAddress = httpProvisionBean.data.address
+                        newMeshJson = FDSMeshApi.instance.updateMeshJsonProvisionerAddress(jsonStr,serverProvisionAddress)
                     }
+                    updateProvisionerAddressListener(newMeshJson)
                 } catch (e: Exception) {
                     e.printStackTrace()
+                    updateProvisionerAddressListener("")
                 }
             } else {
                 LOGUtils.e("Error! 请求服务器上的provisionAddress出错！")
+                updateProvisionerAddressListener("")
             }
         }
     }
